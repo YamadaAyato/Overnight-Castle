a469f5145f4a85314778d1aae9c6832b6021361c
ayato0913a <ayato0913a@gmail.com>
2026-07-28 01:06:09 +0900

    [ add ] 色々対応できるように分離

--- Assets/Person/Yamada/Scripts/Piece/PieceController.cs
+++ Assets/Person/Yamada/Scripts/Piece/PieceController.cs
@@ -1,24 +1,36 @@
 using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 /// <summary>
 ///     プレイヤーの入力に応じてピースを操作するクラス
 /// </summary>
-[RequireComponent(typeof(PlayerInput))]
 public class PieceController : MonoBehaviour
 {
-    private const string MOVE_ACTION_NAME = "Move";
-    private const string ROTATE_ACTION_NAME = "Rotate";
-    private const string DROP_ACTION_NAME = "Drop";
-
     /// <summary> ピースが落下したときに発火するイベント </summary>
     public event Action<FallingPiece> OnPieceDropped;
 
     /// <summary>
+    ///     ピースの移動入力を設定する
+    /// </summary>
+    /// <param name="input">移動入力の値</param>
+    public void SetMoveInput(float input)
+    {
+        _moveInput = Mathf.Clamp(input, -1f, 1f);
+    }
+
+    /// <summary>
+    ///     ピースの回転入力を設定する
+    /// </summary>
+    /// <param name="input">回転入力の値</param>
+    public void SetRotateInput(float input)
+    {
+        _rotateInput = Mathf.Clamp(input, -1f, 1f);
+    }
+
+    /// <summary>
     ///     現在操作中のピースを設定する
     /// </summary>
-    /// <param name="piece"></param>
+    /// <param name="piece">現在操作中のピース</param>
     public void SetCurrentPiece(FallingPiece piece)
     {
         if (piece == null)
@@ -28,6 +40,37 @@ public class PieceController : MonoBehaviour
 
         _currentPiece = piece;
 
+        _moveInput = 0f;
+        _rotateInput = 0f;
+    }
+
+    /// <summary>
+    ///     現在操作中のピースを落下させる
+    /// </summary>
+    public void DropCurrentPiece()
+    {
+        if (!_canControl || _currentPiece == null)
+        {
+            return;
+        }
+
+        // 現在操作中のピースを落下させる
+        FallingPiece droppedPiece = _currentPiece;
+
+        _moveInput = 0f;
+        _rotateInput = 0f;
+        _currentPiece = null;
+
+        droppedPiece.Drop();
+        OnPieceDropped?.Invoke(droppedPiece);
+    }
+
+    /// <summary>
+    ///     プレイヤーによるピースの操作を開始する
+    /// </summary>
+    public void StartControl()
+    {
+        _canControl = true;
         _moveInput = 0f;
         _rotateInput = 0f;
     }
@@ -55,17 +98,12 @@ public class PieceController : MonoBehaviour
     /// <param name="width">ステージの幅</param>
     public void SetStageWidth(float width)
     {
-        _horizontalLimit = width / 2f;
+        _horizontalLimit = Mathf.Max(0f, width / 2f);
     }
 
     [Header("操作設定")]
     [SerializeField, Min(0f), Tooltip("ピースの移動速度")] private float _moveSpeed = 5f;
     [SerializeField, Min(0f), Tooltip("ピースの回転速度")] private float _rotateSpeed = 120f;
-
-    private PlayerInput _playerInput;
-    private InputAction _moveAction;
-    private InputAction _rotateAction;
-    private InputAction _dropAction;
 
     private FallingPiece _currentPiece;
     private float _moveInput;
@@ -74,59 +112,24 @@ public class PieceController : MonoBehaviour
 
     private bool _canControl = true;
 
-    private void Awake()
-    {
-        _playerInput = GetComponent<PlayerInput>();
-
-        _moveAction = _playerInput.actions.FindAction(MOVE_ACTION_NAME, true);
-        _rotateAction = _playerInput.actions.FindAction(ROTATE_ACTION_NAME, true);
-        _dropAction = _playerInput.actions.FindAction(DROP_ACTION_NAME, true);
-    }
-
     private void FixedUpdate()
     {
         // 現在操作中のピースが存在し、操作可能な場合のみ移動と回転を行う
         if (_canControl && _currentPiece != null)
         {
-            Move(_moveInput);
-            Rotate(_rotateInput);
+            Move();
+            Rotate();
         }
-    }
-
-    private void OnEnable()
-    {
-        _moveAction.performed += OnMovePerfomed;
-        _moveAction.canceled += OnMoveCanceled;
-
-        _rotateAction.performed += OnRotatePerformed;
-        _rotateAction.canceled += OnRotateCanceled;
-
-        _dropAction.performed += OnDropPerformed;
-    }
-
-    private void OnDisable()
-    {
-        _moveAction.performed -= OnMovePerfomed;
-        _moveAction.canceled -= OnMoveCanceled;
-
-        _rotateAction.performed -= OnRotatePerformed;
-        _rotateAction.canceled -= OnRotateCanceled;
-
-        _dropAction.performed -= OnDropPerformed;
-
-        _moveInput = 0f;
-        _rotateInput = 0f;
     }
 
     /// <summary>
     ///     ピースを移動する
     /// </summary>
-    /// <param name="input">移動入力値</param>
-    private void Move(float input)
+    private void Move()
     {
         Vector3 position = _currentPiece.transform.position;
 
-        position.x += input * _moveSpeed * Time.fixedDeltaTime;
+        position.x += _moveInput * _moveSpeed * Time.fixedDeltaTime;
         position.x = Mathf.Clamp(position.x, -_horizontalLimit, _horizontalLimit);
         _currentPiece.transform.position = position;
     }
@@ -134,70 +137,11 @@ public class PieceController : MonoBehaviour
     /// <summary>
     ///     ピースを回転する
     /// </summary>
-    /// <param name="input">回転入力値</param>
-    private void Rotate(float input)
+    private void Rotate()
     {
         Vector3 rotation = _currentPiece.transform.eulerAngles;
 
-        rotation.z += input * _rotateSpeed * Time.fixedDeltaTime;
+        rotation.z += _rotateInput * _rotateSpeed * Time.fixedDeltaTime;
         _currentPiece.transform.eulerAngles = rotation;
-    }
-
-    /// <summary>
-    ///     移動入力が行われたときに呼ばれるコールバック
-    /// </summary>
-    /// <param name="context">入力コンテキスト</param>
-    private void OnMovePerfomed(InputAction.CallbackContext context)
-    {
-        _moveInput = context.ReadValue<float>();
-    }
-
-    /// <summary>
-    ///     移動入力がキャンセルされたときに呼ばれるコールバック
-    /// </summary>
-    /// <param name="context">入力コンテキスト</param>
-    private void OnMoveCanceled(InputAction.CallbackContext context)
-    {
-        _moveInput = 0f;
-    }
-
-    /// <summary>
-    ///     回転入力が行われたときに呼ばれるコールバック
-    /// </summary>
-    /// <param name="context">入力コンテキスト</param>
-    private void OnRotatePerformed(InputAction.CallbackContext context)
-    {
-        _rotateInput = context.ReadValue<float>();
-    }
-
-    /// <summary>
-    ///     回転入力がキャンセルされたときに呼ばれるコールバック
-    /// </summary>
-    /// <param name="context">入力コンテキスト</param>
-    private void OnRotateCanceled(InputAction.CallbackContext context)
-    {
-        _rotateInput = 0f;
-    }
-
-    /// <summary>
-    ///     ピースを落下させる入力が行われたときに呼ばれるコールバック
-    /// </summary>
-    /// <param name="context">入力コンテキスト</param>
-    private void OnDropPerformed(InputAction.CallbackContext context)
-    {
-        if (!_canControl || _currentPiece == null)
-        {
-            return;
-        }
-
-        FallingPiece droppedPiece = _currentPiece;
-
-        _moveInput = 0f;
-        _rotateInput = 0f;
-        _currentPiece = null;
-
-        droppedPiece.Drop();
-
-        OnPieceDropped?.Invoke(droppedPiece);
     }
 }
---
+++ Assets/Person/Yamada/Scripts/Piece/PieceInputReader.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PieceInputReader : MonoBehaviour
+{
+    private const string MOVE_ACTION_NAME = "Move";
+    private const string ROTATE_ACTION_NAME = "Rotate";
+    private const string DROP_ACTION_NAME = "Drop";
+
+    [SerializeField] private PieceController _controller;
+
+    private InputAction _moveAction;
+    private InputAction _rotateAction;
+    private InputAction _dropAction;
+
+    private void Reset()
+    {
+        _controller = GetComponent<PieceController>();
+    }
+
+    private void Awake()
+    {
+        if (_controller == null)
+        {
+            _controller = GetComponent<PieceController>();
+        }
+
+        PlayerInput playerInput = GetComponent<PlayerInput>();
+
+        _moveAction = playerInput.actions.FindAction(MOVE_ACTION_NAME, true);
+        _rotateAction = playerInput.actions.FindAction(ROTATE_ACTION_NAME, true);
+        _dropAction = playerInput.actions.FindAction(DROP_ACTION_NAME, true);
+    }
+
+    private void OnEnable()
+    {
+        _moveAction.performed += OnMovePerformed;
+        _moveAction.canceled += OnMoveCanceled;
+
+        _rotateAction.performed += OnRotatePerformed;
+        _rotateAction.canceled += OnRotateCanceled;
+
+        _dropAction.performed += OnDropPerformed;
+    }
+
+    private void OnDisable()
+    {
+        _moveAction.performed -= OnMovePerformed;
+        _moveAction.canceled -= OnMoveCanceled;
+
+        _rotateAction.performed -= OnRotatePerformed;
+        _rotateAction.canceled -= OnRotateCanceled;
+
+        _dropAction.performed -= OnDropPerformed;
+
+        if(_controller != null)
+        {
+            _controller.SetMoveInput(0f);
+            _controller.SetRotateInput(0f);
+        }
+    }
+
+    /// <summary>
+    ///     移動入力が行われたときに呼ばれるコールバック
+    /// </summary>
+    /// <param name="context">入力コンテキスト</param>
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        _controller.SetMoveInput(context.ReadValue<float>());
+    }
+
+    /// <summary>
+    ///     移動入力がキャンセルされたときに呼ばれるコールバック
+    /// </summary>
+    /// <param name="context">入力コンテキスト</param>
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        _controller.SetMoveInput(0f);
+    }
+
+    /// <summary>
+    ///     回転入力が行われたときに呼ばれるコールバック
+    /// </summary>
+    /// <param name="context">入力コンテキスト</param>
+    private void OnRotatePerformed(InputAction.CallbackContext context)
+    {
+        _controller.SetRotateInput(context.ReadValue<float>());
+    }
+
+    /// <summary>
+    ///     回転入力がキャンセルされたときに呼ばれるコールバック
+    /// </summary>
+    /// <param name="context">入力コンテキスト</param>
+    private void OnRotateCanceled(InputAction.CallbackContext context)
+    {
+        _controller.SetRotateInput(0f);
+    }
+
+    /// <summary>
+    ///     ピースを落下させる入力が行われたときに呼ばれるコールバック
+    /// </summary>
+    /// <param name="context">入力コンテキスト</param>
+    private void OnDropPerformed(InputAction.CallbackContext context)
+    {
+        _controller.DropCurrentPiece();
+    }
+}
