6aabc1a0a97f7b84c8fccf4c8f00075f5b5fefbe
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-07-27 15:33:36 +0900

    [add]スコアのマネージャーを作った

---
+++ Assets/Person/Sugimoto/Scripts/Score/ScoreManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+/// <summary>
+/// スコアを管理するクラス
+/// </summary>
+public class ScoreManager : MonoBehaviour
+{
+    /// <summary>現在のスコア</summary>
+    public int CurrentScore { get; private set; }
+    /// <summary>最高スコア</summary>
+    public int MaxScore { get; private set; }
+
+    /// <summary>現在のスコア</summary>
+    public int Score { get; private set; }
+
+    [SerializeField]
+    private ScoreSender _scoreSender;
+
+    /// <summary>
+    /// スコアを加算
+    /// </summary>
+    public void AddScore(int value)
+    {
+        CurrentScore += value;
+
+        if (CurrentScore > MaxScore)
+        {
+            MaxScore = CurrentScore;
+        }
+    }
+
+    /// <summary>
+    /// スコアをリセット
+    /// </summary>
+    public void ResetScore()
+    {
+        Score = 0;
+    }
+
+    /// <summary>
+    /// 最高スコアを送信する
+    /// </summary>
+    public void SendMaxScore()
+    {
+        _scoreSender.SendHighScore(MaxScore);
+    }
+}
