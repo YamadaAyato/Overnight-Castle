a8ca7e2b0ea00fa6fff7f0ea74a0be5a7f34c9ca
ayato0913a <ayato0913a@gmail.com>
2026-07-27 19:50:33 +0900

    [ add ] 全体に一括適用するためのクラス

---
+++ Assets/Person/Yamada/Scripts/Piece/GlobalPiecePhysicsSettings.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+///     すべてのピースへ適用させる場合の物理設定を保持するSO
+/// </summary>
+[CreateAssetMenu(fileName = "GlobalPiecePhysicsSettings", menuName = "Overnight-Castle/GlobalPiecePhysicsSettings")]
+public class GlobalPiecePhysicsSettings : ScriptableObject
+{
+    /// <summary> ピースの物理設定 </summary>
+    public PiecePhysicsSettings PiecePhysicsSettings => _piecePhysicsSettings;
+
+    [SerializeField] private PiecePhysicsSettings _piecePhysicsSettings = new();
+}
