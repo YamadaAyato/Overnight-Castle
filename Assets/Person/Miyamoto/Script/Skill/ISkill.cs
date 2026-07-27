e567dac9b99481d254d17d31f8f43c228b04060c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-28 05:41:53 +0900

    [add]クラス追加

---
+++ Assets/Person/Miyamoto/Script/Skill/ISkill.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+///     スキルのインターフェース
+/// </summary>
+public interface ISkill
+{
+    /// <summary>
+    ///     スキルの効果
+    /// </summary>
+    public void SkillEffect();
+}
---
+++ Assets/Person/Miyamoto/Script/Skill/TestSkill.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+///     手本となるスキルの書き方
+/// </summary>
+[System.Serializable]
+public class TestSkill : ISkill
+{
+    [SerializeField] private StageSettings _stageSettings;
+    public void SkillEffect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
