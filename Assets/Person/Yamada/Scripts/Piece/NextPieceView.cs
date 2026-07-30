53d6c33b3a4ceb38a222b12e55b12d492e669cf3
Sawae-M <mikann22442244@gmail.com>
2026-07-31 03:03:05 +0900

    [ add ]NextPieceViewに次のピース表示の演出を追加

--- Assets/Person/Yamada/Scripts/Piece/NextPieceView.cs
+++ Assets/Person/Yamada/Scripts/Piece/NextPieceView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 /// <summary>
 ///     次に出現するピースの画像を表示するビュー
@@ -8,6 +9,10 @@ public class NextPieceView : MonoBehaviour
 {
     [SerializeField] private PieceSpawner _pieceSpawner;
     [SerializeField] private Image _image;
+    [SerializeField] private float _moveDistance = 60f;
+    [SerializeField] private float _duration = 0.12f;
+
+    private Vector2 _basePos;
 
     private void OnEnable()
     {
@@ -16,8 +21,11 @@ public class NextPieceView : MonoBehaviour
             Debug.LogError("PieceSpawnerが設定されていません。");
             return;
         }
+
+        _basePos = _image.rectTransform.anchoredPosition;
         _pieceSpawner.OnNextPieceDefinitionChanged += ShowNextPiece;
-        ShowNextPiece(_pieceSpawner.NextPieceDefinition);
+        SetSprite(_pieceSpawner.NextPieceDefinition);
+        //ShowNextPiece(_pieceSpawner.NextPieceDefinition);
     }
 
     private void OnDisable()
@@ -26,6 +34,9 @@ public class NextPieceView : MonoBehaviour
         {
             _pieceSpawner.OnNextPieceDefinitionChanged -= ShowNextPiece;
         }
+
+        DOTween.Kill(_image.rectTransform);
+        //_image.rectTransform.anchoredPosition = _basePos;
     }
 
     /// <summary>
@@ -34,6 +45,17 @@ public class NextPieceView : MonoBehaviour
     /// <param name="pieceDefinition"></param>
     private void ShowNextPiece(PieceDefinition pieceDefinition)
     {
+        var rt = _image.rectTransform;
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(rt.DOAnchorPosY(_basePos.y + _moveDistance, _duration));
+        sequence.AppendCallback(() =>
+        {
+            SetSprite(pieceDefinition);
+            rt.anchoredPosition = new Vector2(_basePos.x, _basePos.y - _moveDistance);
+        });
+        sequence.Append(rt.DOAnchorPosY(_basePos.y, _duration));
+        /*
         if (_image == null)
         {
             return;
@@ -48,6 +70,12 @@ public class NextPieceView : MonoBehaviour
         }
 
         _image.sprite = pieceDefinition.Sprite;
-        _image.enabled = true;
+        _image.enabled = true; */
+    }
+    private void SetSprite(PieceDefinition pieceDefinition)
+    {
+        bool hasSprite = pieceDefinition != null && pieceDefinition.Sprite != null;
+        _image.sprite = hasSprite ? pieceDefinition.Sprite : null;
+        _image.enabled = hasSprite;
     }
 }
