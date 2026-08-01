2b802f8b4b07fee3876265060175a5da25c804bb
ayato0913a <ayato0913a@gmail.com>
2026-08-01 20:15:27 +0900

    [ add ] 削除方法のenum追加

---
+++ Assets/Person/Yamada/Scripts/Piece/BoardPieceTargetMode.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+///     盤面ピースを対象にするモードを定義する列挙型
+/// </summary>
+public enum BoardPieceTargetMode
+{
+    /// <summary> 指定された数のピースを対象にするモード </summary>
+    SpecifiedCount,
+    /// <summary> すべてのピースを対象にするモード </summary>
+    All
+}
